588470837e2b04501747362bf923796f1373bf91
Led <ledest@gmail.com>
2017-04-30 20:48:51 +0300

    lopt_pt: add copywrite

--- src/lopt_pt.erl
+++ src/lopt_pt.erl
@@ -1,7 +1,24 @@
+%%% Copyright 2017 Oleksandr Chumachenko <ledest@gmail.com>
+%%%
+%%% This file is part of LOpt.
+%%%
+%%% LOpt is free software: you can redistribute it and/or modify it
+%%% under the terms of the GNU General Public License as published by
+%%% the Free Software Foundation, either version 3 of the License, or
+%%% (at your option) any later version.
+%%%
+%%% LOpt is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+%%% See the GNU General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with LOpt. If not, see <http://www.gnu.org/licenses/>.
+
 -module(lopt_pt).
 
 -export([parse_transform/2]).
 
 -spec parse_transform(Forms::[erl_syntax:syntaxTree()], proplists:proplist()) -> [erl_syntax:syntaxTree()].
 parse_transform(Forms, Options) ->
-    lists:foldl(fun(M, A) -> M:parse_transform(A, Options) end, Forms, [const_pt, inline_pt]).
+    lists:foldl(fun(M, A) -> M:parse_transform(A, Options) end, Forms, [const_pt, inline_pt, lists_pt]).
